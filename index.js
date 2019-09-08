83933536fec7561436f483bd4b75edd7bd20545f
candescent-media-9936 <alexbonwit@mac.com>
2019-09-08 22:44:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function logWhisper(string) {
   console.log( string.toLowerCase() );
 }
 
+function sayHiToGrandma(string) {
+  if (string == 'I love you, grandma.') {
+    return 'I love you, too.';
+  }
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+  }
+}
